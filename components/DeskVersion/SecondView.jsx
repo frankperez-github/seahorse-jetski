d96499372b3eed3608fcb60448e2ec58d449e2c2
Frank Pérez <fp848584@gmail.com>
2021-07-08 18:56:02 -0400

    Upgrading Video

--- components/DeskVersion/SecondView.jsx
+++ components/DeskVersion/SecondView.jsx
@@ -3,11 +3,12 @@
 export default function Secondview(){
     return(
         <div className="Second">
-                    
-            <video className="video" controls >
-                <source src="/vid1.mp4" />
-            </video>
-      
+            
+           <div className="video">
+           <iframe width="650" height="355" src="https://www.youtube.com/embed/bARCE45FM4s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           </div>
+            
+            
         </div>
     );
 }
