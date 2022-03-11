13f46d5d6b261e6fe832127b5aa99ea45dc74224
Frank Perez <fp848584@gmail.com>
2022-03-11 12:59:45 -0500

    Video Uploaded

--- components/DeskVersion/SecondView.jsx
+++ components/DeskVersion/SecondView.jsx
@@ -5,7 +5,7 @@ export default function Secondview(){
         <div className="Second">
             
            <div className="video">
-           <iframe width="650" height="355" src="https://www.youtube.com/embed/bARCE45FM4s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <iframe width="650" height="355" src="https://www.youtube.com/embed/axvLSW8GPp4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            </div>
             
             
